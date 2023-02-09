08d8d0df57aad35cb67760326c71b509e460cd42
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-02-09 13:19:26 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import main;
-
-def test1():
-    assert main.remove_underscore(add_underscore(add_hash('Python'))) == 'Python'
-def test2():
-    assert main.remove_underscore(add_underscore(add_hash('Hello World'))) == 'HelloWorld'
-def test3():
-    assert main.remove_underscore(add_underscore(add_hash('Foo Bar Baz'))) == 'FooBarBaz'
